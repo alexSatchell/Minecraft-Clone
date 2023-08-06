cfe80c292f66b8d4fd2517628d67402cbcb813c1
Alexander Satchell <48191535+alexSatchell@users.noreply.github.com>
2023-08-06 14:37:12 -0400

    Add project files.

---
+++ Modern OpenGL/maincpp.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+// Callback function declarations
+// ------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+int main()
+{
+	// Instantiate GLFW window
+	// -----------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create window object
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Triangles Are Hard", NULL, NULL);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		// destroy all existing windows & cursors
+		glfwTerminate();
+		return -1;
+	}	
+	// Ensures openGL will flush all the rendering results to the specified window.
+	glfwMakeContextCurrent(window);
+	
+	//Initialize GLAD (opengl function pointers)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init GLAD" << std::endl;
+		return -1;
+	};
+
+	// Callback functions
+	// ------------------
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// Render Loop
+	//------------
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clean/Delete all GLFW resources
+	glfwTerminate();
+	return 0;
+}
+
+// Callback Function Definitions
+// -----------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+
+	if (key == GLFW_KEY_F)
+	{
+		if (action == GLFW_PRESS)
+		{
+			std::cout << "Respects paid." << std::endl;
+		}
+	}
+}
+
+// check if cursor over the window
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	// Hide mouse if over the window
+	if (glfwGetWindowAttrib(window, GLFW_HOVERED))
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+		glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+	}
+	// std::cout << "x: " << xpos << ", y: " << ypos << std::endl;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	std::cout << "scrollX: " << xoffset << ", scrollY: " << yoffset << std::endl;
+}
+
