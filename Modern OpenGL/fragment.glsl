7a10f9d2230595ec970cb4574aff911243c2ec74
Alexander Satchell <48191535+alexSatchell@users.noreply.github.com>
2023-08-22 06:57:36 -0400

    We have a gold star (texturing implemented)

--- Modern OpenGL/fragment.glsl
+++ Modern OpenGL/fragment.glsl
@@ -1,9 +1,11 @@
 #version 330 core
-in vec4 fColor;
+in vec2 fTexCoord;
 
 out vec4 FragColor;
 
+uniform sampler2D ourTexture;
+
 void main()
 {
-	FragColor = fColor;
+	FragColor = texture(ourTexture, fTexCoord);
 }
--- Modern OpenGL/maincpp.cpp
+++ Modern OpenGL/maincpp.cpp
@@ -5,7 +5,9 @@
 #include <GLFW/glfw3.h>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
+#include <glm/vec2.hpp>
 #include "Shader.h"
+#include <stb_image.h>
 
 const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080;
@@ -51,7 +53,8 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetCursorPosCallback(window, cursor_position_callback);
 	glfwSetScrollCallback(window, scroll_callback);
-	
+
+
 	// SHADERS
 	// -------
 	Shader shaderProgram("vertex.glsl", "fragment.glsl");
@@ -62,32 +65,33 @@ int main()
 	{
 		glm::vec3 position;
 		glm::vec4 color;
+		glm::vec2 texture;
 	};
 
 	// Data for star
 	Vertex vertices[10]
 	{
-		{ glm::vec3(0.0f, 0.85f, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f)}, // Top
-		{ glm::vec3(0.1f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f)}, // Top Right
-		{ glm::vec3(0.3f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f)}, // Top Far Right
-		{ glm::vec3(0.15f, 0.0f, 1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)}, // Middle Right 
-		{ glm::vec3(0.25f, -0.5f, 1.0f), glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)}, // Bottom Right 
-		{ glm::vec3(0.0f, -0.2f, 1.0f), glm::vec4(0.0f, 0.5f, 0.5f, 1.0f)}, // Bottom Middle 
-		{ glm::vec3(-0.25f, -0.5f, 1.0f), glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)}, // Bottom Left 
-		{ glm::vec3(-0.15f, 0.0f, 1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)}, // Middle Left 
-		{ glm::vec3(-0.3f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f)}, // Top Far Left 
-		{ glm::vec3(-0.1f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f)}, // Top Left
+		{ glm::vec3(0.0f, 0.85f, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), glm::vec2(0.5f, 1.0f)}, // Top
+		{ glm::vec3(0.1f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f), glm::vec2(0.75f, 0.75f)}, // Top Right
+		{ glm::vec3(0.3f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.75f)}, // Top Far Right
+		{ glm::vec3(0.15f, 0.0f, 1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(0.75f, 0.5f)}, // Middle Right 
+		{ glm::vec3(0.25f, -0.5f, 1.0f), glm::vec4(0.0f, 0.0f, 1.0f, 1.0f), glm::vec2(0.85f, 0.0f)}, // Bottom Right 
+		{ glm::vec3(0.0f, -0.2f, 1.0f), glm::vec4(0.0f, 0.5f, 0.5f, 1.0f), glm::vec2(0.5f, 0.0f)}, // Bottom Middle 
+		{ glm::vec3(-0.25f, -0.5f, 1.0f), glm::vec4(0.0f, 0.0f, 1.0f, 1.0f), glm::vec2(0.15f, 0.0f)}, // Bottom Left 
+		{ glm::vec3(-0.15f, 0.0f, 1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(0.25f, 0.5f)}, // Middle Left 
+		{ glm::vec3(-0.3f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.75)}, // Top Far Left 
+		{ glm::vec3(-0.1f, 0.3f, 1.0f), glm::vec4(0.5f, 0.0f, 0.0f, 1.0f), glm::vec2(0.25f, 0.75f) }, // Top Left
 	};
 
-	unsigned int indices [24]
+	unsigned int indices[24]
 	{
-		0, 1, 9, 
+		0, 1, 9,
 		1, 2, 3,
 		3, 4, 5,
-		5, 6, 7, 
-		7, 8, 9, 
+		5, 6, 7,
+		7, 8, 9,
 		9, 1, 7,
-		7, 5, 3, 
+		7, 5, 3,
 		3, 1, 7
 	};
 
@@ -99,7 +103,7 @@ int main()
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+
 	GLuint EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -127,8 +131,41 @@ int main()
 	);
 	glEnableVertexAttribArray(1);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(7 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(2);
+
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// TEXTURES
+	//---------
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("Metal042A_1K_Color.png", &width, &height, &nrChannels, 0);	
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	// de-allocate memory for image data 
+	stbi_image_free(data);
+
+
 	// RENDER LOOP
 	//------------
 	while (!glfwWindowShouldClose(window))
@@ -140,6 +177,9 @@ int main()
 			(1.0f));
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Bind Texture
+		glBindTexture(GL_TEXTURE_2D, texture);
+
 		// Activate shader program
 		glUseProgram(shaderProgram.ID);
 
@@ -148,9 +188,9 @@ int main()
 		// glUniform1f(horizontalOffset, .5);
 
 		// Render Vertex Data
-		glBindVertexArray(VAO);	
+		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
-		
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
